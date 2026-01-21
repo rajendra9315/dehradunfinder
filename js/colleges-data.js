f6d882516c41474190b6f41d5b6d98ef176d9925
Rajendra 5 <rawatrajendra9315@gmail.com>
2026-01-21 15:24:29 +0530

    Create colleges-data.js

---
+++ js/colleges-data.js
@@ -0,0 +1,12 @@
+const colleges = [
+  { name: "UPES Dehradun", slug: "upes-dehradun" },
+  { name: "Graphic Era University", slug: "graphic-era-university" },
+  { name: "DIT University", slug: "dit-university" },
+  { name: "Uttaranchal University", slug: "uttaranchal-university" },
+  { name: "Doon University", slug: "doon-university" },
+  { name: "IMS Unison University", slug: "ims-unison-university" },
+  { name: "Tula's Institute", slug: "tulas-institute" },
+  { name: "JB Institute of Technology", slug: "jb-institute-of-technology" },
+  { name: "Sardar Bhagwan Singh University", slug: "sbs-university" },
+  { name: "ICFAI University Dehradun", slug: "icfai-university-dehradun" }
+];
